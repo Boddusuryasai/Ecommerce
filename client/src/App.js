9987d067f2740dae43c96104ced7e4b9bf2f2dc3
suryasai <suryasaichanti@gmail.com>
2023-04-06 14:47:03 +0530

    initial setup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <>
+    <Layout>
+      <h1 >hello</h1>
+    </Layout>
+    </>
   );
 }
 
---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='py-4 text-white text-center  font-bold bg-blue-950'>
+        <p>All rights reserved by @suryasai</p>
+    </div>
+  )
+}
+
+export default Footer
