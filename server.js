c58bf8d7178213b0eb5383b4a7b51f594e262288
suryasai <suryasaichanti@gmail.com>
2023-04-06 13:35:49 +0530

    register route completed

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectToDB from "./config/db.js"
+import authRoutes from "./routes/authRoutes.js"
 const app = express()
 dotenv.config()
 connectToDB()
+
+app.use(express.json())
+app.use("/api/v1/auth" , authRoutes)
 app.get("/" ,
 (req,res)=>{
 res.send("server home")
