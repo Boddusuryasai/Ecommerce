c5887af07a7c2f14d0d5382263985a18f9273761
suryasai <suryasaichanti@gmail.com>
2023-04-07 16:05:07 +0530

    products crud completed

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from "cors"
 import connectToDB from "./config/db.js"
 import authRoutes from "./routes/authRoutes.js"
 import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 const app = express()
 dotenv.config()
 connectToDB()
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(cors())
 app.use("/api/v1/auth" , authRoutes)
 app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 app.get("/" ,
 (req,res)=>{
 res.send("server home")
