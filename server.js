42ffd42865c57adb2597f3a6b7146cff7dbf6e5d
suryasai <suryasaichanti@gmail.com>
2023-04-07 15:29:29 +0530

    category crud api completed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import cors from "cors"
 import connectToDB from "./config/db.js"
 import authRoutes from "./routes/authRoutes.js"
+import categoryRoutes from "./routes/categoryRoutes.js";
 const app = express()
 dotenv.config()
 connectToDB()
@@ -10,6 +11,7 @@ connectToDB()
 app.use(express.json())
 app.use(cors())
 app.use("/api/v1/auth" , authRoutes)
+app.use("/api/v1/category", categoryRoutes);
 app.get("/" ,
 (req,res)=>{
 res.send("server home")
